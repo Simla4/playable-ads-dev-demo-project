0d8f42bf102aaef38fcd5cc89cc943a0c5bbdba5
Simla4 <simlabeken4@gmail.com>
2025-10-29 17:20:05 +0300

    changed folder name

---
+++ Assets/GAME/Scripts/Managers/CustomerManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> customers;
+    
+    
+}
